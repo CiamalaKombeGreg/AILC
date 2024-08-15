891b63152daf6f155b42e77814073f39e46fb4d4
CiamalaKombeGreg <he202098@students.ephec.be>
2024-08-15 19:01:06 +0200

    Modification de la variable contenant les fichiers + encodages des PRE/POST méthodes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Librairies
+# Librairies
 """
     Toutes les librairies et importations utilisées
 """
@@ -6,27 +6,29 @@
 import os
 import cmd
 import sys
-from pervision import *
+from pervision import analyse
 
-#classe
+# classe
+
 
 def cls():
     """
-        Cette fonction permet de clear la l'interface en ligne de commande
+    Cette fonction permet de clear la l'interface en ligne de commande
     """
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 class mainProg(cmd.Cmd):
     cls()
     analyse(sys.argv[1])
     intro = "Pervision 1.0   Tapez help ou ? to pour afficher la liste des commandes.\n"
-    prompt = sys.argv[1]+" > "
+    prompt = sys.argv[1] + " > "
     file = None
 
     # ----- basic enquete commands -----
 
     def do_pervision(self, arg):
-        '\n{logs} - Afficher le fichier log.\n\n{fichiers} - Afficher le fichier CSV.\n'
+        "\n{logs} - Afficher le fichier log.\n\n{fichiers} - Afficher les fichiers.\n"
 
         cls()
         if arg == "logs":
@@ -34,27 +36,34 @@ class mainProg(cmd.Cmd):
         elif arg == "fichiers":
             print("fichiers")
         else:
-            print("Argument erroné, faites <help pervision> pour plus d'informations.\n")
-    
+            print(
+                "Argument erroné, faites <help pervision> pour plus d'informations.\n"
+            )
+
     def do_ls(self, arg):
-        'Afficher le répertoire'
-        
+        "Afficher le répertoire"
+
         cls()
-        print(os.system('dir '+sys.argv[1]))
+        print(os.system("dir " + sys.argv[1]))
 
     def do_fermer(self, arg):
-        'Ferme le programme'
-        
+        "Ferme le programme"
+
         cls()
         print("Merci et à bientôt!")
         return True
 
-#Corps principale
 
-if __name__ == '__main__':
-    if len(sys.argv) == 2: # On verifie si il n'y a que deux argument, l'application et le chemin du répertoire
-        if os.path.exists(sys.argv[1]): # On verifie que le réperoire existe
-            if os.path.isdir(sys.argv[1]): # On verifie si nous avons bien un répertoire
+# Corps principale
+
+if __name__ == "__main__":
+    if (
+        len(sys.argv) == 2
+    ):  # On verifie si il n'y a que deux argument, l'application et le chemin du répertoire
+        if os.path.exists(sys.argv[1]):  # On verifie que le réperoire existe
+            if os.path.isdir(
+                sys.argv[1]
+            ):  # On verifie si nous avons bien un répertoire
                 mainProg().cmdloop()
             else:
                 print("Ce n'est pas un répertoire.")
--- pervision.py
+++ pervision.py
@@ -9,16 +9,29 @@ from datetime import datetime
 
 # Variables
 
-files = []
+files = {}  # Les classes files seront encodées dans cette variable
 
 # Classes
 
 
-class file:
-    def __init__(self, nom, dateC, dateM, taille):
+class File:
+    """
+    Cette classe représente un fichier existant dans le répertoire choisi
+
+    Ces fichiers permettront de comparer les changements présents dans le répertoires
+    """
+
+    def __init__(self, nom, datec, datem, taille):
+        """
+        Initialise une instance de la classe File.
+
+        PRE : Aucune.
+        POST : Crée un nouveau objet 'File' avec les informations spécifiées ...
+        """
+
         self.__nom = nom
-        self.__datec = dateC
-        self.__datem = dateM
+        self.__datec = datec
+        self.__datem = datem
         self.__taille = taille
 
     def __str__(self):
@@ -26,34 +39,70 @@ class file:
 
     @property
     def nom(self):
+        """Cette fonction retourne le nom de l'objet"""
+
         return self.__nom
 
     @nom.setter
     def nom(self, new_nom):
+        """
+        Modifie le nom de l'objet avec une nouvelle valeur.
+
+        PRE : Aucune.
+        POST :  Modifie le nom avec la nouvelle valeur...
+        """
+
         self.__nom = new_nom
 
     @property
     def datec(self):
+        """Cette fonction retourne la date de création de l'objet"""
+
         return self.__datec
 
     @datec.setter
     def datec(self, new_date):
+        """
+        Modifie la date de création de l'objet avec une nouvelle valeur.
+
+        PRE : Aucune.
+        POST :  Modifie la date de création avec la nouvelle valeur...
+        """
+
         self.__datec = new_date
 
     @property
     def datem(self):
+        """Cette fonction retourne la date de mise à jour de l'objet"""
+
         return self.__datem
 
     @datem.setter
     def datem(self, new_date):
+        """
+        Modifie la date de mise à jour de l'objet avec une nouvelle valeur.
+
+        PRE : Aucune.
+        POST :  Modifie la date de mise à jour avec la nouvelle valeur...
+        """
+
         self.__datem = new_date
 
     @property
     def taille(self):
+        """Cette fonction retourne la taille de l'objet"""
+
         return self.__taille
 
     @taille.setter
     def taille(self, new_taille):
+        """
+        Modifie la taille de l'objet avec une nouvelle valeur.
+
+        PRE : Aucune.
+        POST :  Modifie la taille avec la nouvelle valeur...
+        """
+
         self.__taille = new_taille
 
 
@@ -63,7 +112,7 @@ class file:
 def analyse(chemin):
     """analyse les fichiers du répertoire choisi et encode les métadonnées dans le fichier CSV
 
-    PRE : Prend le chemin du répertoire comme paramètre 'chemin'
+    PRE : Prend le chemin du répertoire qui est valide comme paramètre 'chemin'
 
     POST : Retranscrit les métadonnées (nom, date de création, date
            de mise à jour et taille) de tous les fichiers dans un fichier CSV
@@ -89,15 +138,20 @@ def analyse(chemin):
             ).strftime(
                 "%Y-%m-%d %H:%M:%S"
             )  # Date de mise à jour
-            files.append(file(nom_fichier, date_creation, date_modification, taille))
+            files[nom_fichier] = File(
+                nom_fichier, date_creation, date_modification, taille
+            )
 
     try:
+        # On ouvre le fichier meta.csv en écriture
         with open("meta.csv", "w", newline="", encoding="utf-8") as csvfile:
+            # On lie le contenu du fichier à un objet qui est représenter par uen variable
             spamwriter = csv.writer(
                 csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL
             )
-            for f in files:
-                spamwriter.writerow([f.nom, f.datec, f.datem, f.taille])
+            for f in files.items():
+                # On encode les métadonnées des fichier, un fichier par ligne.
+                spamwriter.writerow([f[1].nom, f[1].datec, f[1].datem, f[1].taille])
     except FileNotFoundError as e:
         print("Le fichier n'as pas été trouvée :" + e)
     except IOError as e:
